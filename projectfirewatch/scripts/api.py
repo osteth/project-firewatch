2b6104fdc687d865c681639c79fc6160c3c5e01d
osteth <osteth@gmail.com>
2017-04-29 14:24:51 -0500

    API changes begin

--- projectfirewatch/scripts/api.py
+++ projectfirewatch/scripts/api.py
@@ -6,23 +6,13 @@ import json
 app = FlaskAPI(__name__)
 
 @app.route("/api/", methods=['GET', 'POST'])
-def Player_Checkin():
+def Dump_sat():
 	'''
 	curl -H "Content-Type: application/json" -X POST -d '{"TeamID": "Mudkips","IP": "192.168.0.1","Port": "5001"}' http://localhost:5001/checkin/
 	'''
-	try:
-		checkin_resp = request.data
-		IP = checkin_resp.get('IP')
-		Port  = checkin_resp.get('Port')
-		TeamID  = checkin_resp.get('TeamID')
-		if IP and Port and TeamID is not None:
-			Token, key = CheckinUpdate(IP, Port, TeamID)
-			return {'Score Token': Token,
-					'Key': key}
-		else:
-			return {'Invalid': 'request'}
-	except:
-		return {'Invalid': 'request'}
+	SatDAta= json.load(data.json)
+	return {'Satalite data': SatDAta}
+
 
 
 if __name__ == "__main__":
