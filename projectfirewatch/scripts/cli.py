08d658107655a0e22354b80c127b3f679fd07adc
osteth <osteth@gmail.com>
2017-05-02 22:08:03 -0500

    add new llr to cli

--- projectfirewatch/scripts/cli.py
+++ projectfirewatch/scripts/cli.py
@@ -72,29 +72,33 @@ cli.add_command(start)
 
 #begin Auxiliary Functions
 def LLR():
-    llr_dict = {}
+	table = []
+	llr_table = []
 
-	# Reading data back
-    with open('MODIS_C6_Global_24h.json') as json_file:
-        for string in json_file:
-            data = json.loads(string)
-            lon = float(data['longitude'])
-            lat = float(data['latitude'])
-            scan = data['scan']
-            track = data['track']
-            radius = round((float(scan) * float(track) / 2) * 750, 2) #kilometers rounded to two decimal places 
-            stroke_color = "FF0000"
-            fill_color = "FF0000"
-            llr_dict.update({'stroke_color': 'FF0000',
-            'fill_color': 'FF0000',
-            'center': {'longitude': lon,
-            'latitude': lat},
-            'radius': radius,
-            'infobox': 'Activate Fire Area as reporterd'})
-    '''for i in llr_dict:
-        print (i, llr_dict[i])
-            #print(stroke_color,fill_color,lon,lat,radius)'''
-    return llr_dict
+	with open('MODIS_C6_Global_24h.json', 'r') as data_file:
+		for line in data_file:
+			try:
+				j = line.split('|')[-1]
+				table.append(json.loads(j))
+			except ValueError:
+				print("Bad Json File!")
+				continue
+
+	for row in table:
+		lon = float(row['longitude'])
+		lat = float(row['latitude'])
+		scan = row['scan']
+		track = row['track']
+		radius = (float(scan) * float(track) / 2) * 750 #kilometers
+		radius = round(radius, 2) #round to two decimal places
+		stroke_color = "FF0000"
+		fill_color = "FF0000"
+		llr_table.append({
+		'center': {'longitude': lon,
+		'latitude': lat},
+		'radius': radius,
+		'infobox': 'Activate Fire Area as reported'})
+	return(llr_table)
 	
 #End Auxilary Functions
 
